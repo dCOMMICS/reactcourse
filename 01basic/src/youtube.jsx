74dd5efb4cbe1746f576e4058a14588601ca34c6
dCOMMICS <sanwurld@gmail.com>
2023-09-12 23:22:06 +0300

    nothing changed

--- 01basic/src/youtube.jsx
+++ 01basic/src/youtube.jsx
@@ -1,10 +1,13 @@
-import YoutubePlayer from "./youtube";
-function App (){
-    return (
-        <>
-        <h1>YoutubePlayer</h1>
-        <Youtube />
-        </>
-    );
+import React from "react";
+import { Youtube } from "./youtube"; // Adjust the import statement if needed.
+
+function App() {
+  return (
+    <>
+      <h1>Stupid</h1>
+      <Youtube />
+    </>
+  );
 }
-export default YoutubePlayer;
+
+export default App;
