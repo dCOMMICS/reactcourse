25e4fc5a06473ff856758f4ad5d806757e6a1b7b
dCOMMICS <sanwurld@gmail.com>
2023-09-12 23:55:15 +0300

    done for the day

---
+++ jsx/customreact.js
@@ -0,0 +1,10 @@
+const reactElement ={
+    type:'a'
+    props:{
+        href: 'http://google.com",
+        target: '_blank',
+    },
+
+    children:'Click Me to visit Google',
+}
+document.getElementById('#root')
